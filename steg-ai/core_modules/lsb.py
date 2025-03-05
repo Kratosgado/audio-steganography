639c064c30dd28882a67e740297145b407f71082
Kratosgado <mbeahessilfieprince@gmail.com>
2025-03-05 10:25:50 +0000

    first lsb

---
+++ steg-ai/core_modules/lsb.py
@@ -0,0 +1,92 @@
+from typing import List, Union
+import numpy as np
+import soundfile as sf
+import librosa
+
+
+class AudioSteganography:
+    def __init__(self, embedding_method="lsb"):
+        """
+        Initialize steganography module with selected embedding method.
+
+        Args:
+            embedding_method (str): Method of embedding ('lsb' , 'spread_spectrum')
+        """
+        self.embedding_method = embedding_method
+
+    def text_to_binary(self, message: str) -> List[int]:
+        """
+        Convert text message to binary representation
+        Args:
+            message (str): Input message to encode
+        Returns:
+            binary_message (List[int]): Binary representation of input message
+        """
+        return [int(bit) for char in message for bit in bin(ord(char))[2:].zfill(8)]
+
+    def lsb_embed(self, audio_data: np.ndarray, message: str) -> np.ndarray:
+        """
+        Embed message in audio data using LSB method
+        Args:
+            audio_data (np.ndarray): Audio data to embed message in
+            message (str): Message to embed in audio data
+        Returns:
+            stego_audio (np.ndarray): Audio data with embedded message
+        """
+        binary_message = self.text_to_binary(message)
+
+        # Check if message can be embedded in audio data
+        if len(binary_message) > len(audio_data):
+            raise ValueError("Message is too long to be embedded in audio data")
+
+        stego_audio = audio_data.copy()
+
+        # Embed message in audio data using LSB method
+        for i, bit in enumerate(binary_message):
+            stego_audio[i] = (stego_audio[i] & 0xFE) | bit
+        return stego_audio
+
+    def lsb_extract(self, audio_data: np.ndarray, msg_length: int) -> str:
+        """
+        Extract message from audio data using LSB method
+        Args:
+            audio_data (np.ndarray): Audio data to extract message from
+            msg_length (int): Length of message to extract
+        Returns:
+            extracted_message (str): Extracted message from audio data
+        """
+        extracted_bit = [audio_data[i] & 1 for i in range(msg_length)]
+
+        # Convert binary representation to text message
+        binary_str = "".join(map(str, extracted_bit))
+        message = "".join(
+            chr(int(binary_str[i : i + 8], 2)) for i in range(0, len(binary_str), 8)
+        )
+        return message
+
+    def load_audio(self, file_path: str) -> np.ndarray:
+        """
+        Load audio file and convert to numpy array
+
+        Args:
+            file_path (str): Path to audio file
+
+        Returns:
+            np.ndarray: Audio data
+        """
+        audio_data, sample_rate = librosa.load(file_path)
+        return audio_data
+
+    def save_audio(
+        self, audio_data: np.ndarray, file_path: str, sample_rate: int = 22050
+    ):
+        """
+        Save audio data to file
+
+        Args:
+            audio_data (np.ndarray): Audio numpy array
+            file_path (str): Output file path
+            sample_rate (int): Audio sample rate
+        """
+
+        sf.write(file_path, audio_data, sample_rate)
--- steg-ai/main.py
+++ steg-ai/main.py
@@ -1,3 +1,10 @@
-x = 5
+from core_modules.lsb import AudioSteganography
 
-print("Hello, World!")
+if __name__ == "__main__":
+    steg = AudioSteganography()
+    original_audio = steg.load_audio("input_audio.flac")
+    stego_audio = steg.lsb_embed(original_audio, "Hello, World!")
+
+    steg.save_audio(stego_audio, "output_audio.wav")
+    extracted_message = steg.lsb_extract(stego_audio, len("Hello, World!"))
+    print(extracted_message)
