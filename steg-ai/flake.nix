21e694d30d8a5c7e4d3e30c89c763781a24f5a6a
Kratosgado <mbeahessilfieprince@gmail.com>
2025-03-12 19:32:17 +0000

    flake

---
+++ steg-ai/flake.nix
@@ -0,0 +1,18 @@
+
+{
+  description = "Python + NumPy shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShell.${system} = pkgs.mkShell {
+        buildInputs = [
+          pkgs.python311
+          pkgs.python311Packages.numpy
+        ];
+      };
+    };
+}
