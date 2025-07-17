7b2379b12548ac41fb7fe39c597e824e0c876d29
Kratosgado <mbeahessilfieprince@gmail.com>
2025-07-17 12:12:20 +0000

    fix: flake.nix

--- backend/flake.nix
+++ backend/flake.nix
@@ -10,17 +10,14 @@
       devShell.${system} = pkgs.mkShell {
         buildInputs = with pkgs; [
           python312
-          python312Packages.numpy
           python312Packages.librosa
           python312Packages.scipy
           python312Packages.fastapi
           python312Packages.uvloop
           python312Packages.stable-baselines3
-          python312Packages.shimmy
+          # python312Packages.shimmy
           python312Packages.soundfile
-          python312Packages.matplotlib
           python312Packages.torch
-          python312Packages.scikit-learn
         ];
       };
     };
